0a6dfdabd8f7d1801f293a0debec9efdf9eceaf8
Chris Stephens <chris.stephens@appliedtechnology.se>
2024-01-05 17:32:01 +0100

    update: Dbs

--- BillionnaireShoppingCart.Api/Db/BillionnairesDb.cs
+++ BillionnaireShoppingCart.Api/Db/BillionnairesDb.cs
@@ -9,17 +9,17 @@ public class BillionnairesDb : IDatabase<Billionnaire>
     {
         new () {
             Name = "Elon Musk",
-            NetWorth = 251000000000,
+            NetWorth = 2567398680000,
             ImagePath = "https://imageio.forbes.com/specials-images/imageserve/62d700cd6094d2c180f269b9/0x0.jpg?format=jpg&crop=959,959,x0,y0,safe"
         },
         new () {
             Name = "Jeff Bezos",
-            NetWorth = 161000000000,
+            NetWorth = 1646817480000,
             ImagePath = "https://imageio.forbes.com/specials-images/imageserve/5bb22ae84bbe6f67d2e82e05/0x0.jpg?format=jpg&crop=1012,1013,x627,y129,safe"
         },
         new () {
             Name = "Larry Ellison",
-            NetWorth = 158000000000,
+            NetWorth = 1616131440000,
             ImagePath = "https://specials-images.forbesimg.com/imageserve/5e8b62cfc095010007bffea0/416x416.jpg?background=000000&cropX1=0&cropX2=4529&cropY1=652&cropY2=5184"
         }
         
--- BillionnaireShoppingCart.Api/Db/ProductsDb.cs
+++ BillionnaireShoppingCart.Api/Db/ProductsDb.cs
@@ -7,9 +7,9 @@ public class ProductsDb : IDatabase<Product>
     public List<Product> Data { get; set; } = new()
     {
         new () {
-            Name = "Product1",
-            Price = 1000,
-            ImagePath = "http://example.com"
+            Name = "Beer 0.4L",
+            Price = 64,
+            ImagePath = "https://www.spendrups.se/ConvertedImages/19e78202-73ef-4d31-b9d8-8e6fdad8dca3.png?format=jpg"
         }
     };
 }

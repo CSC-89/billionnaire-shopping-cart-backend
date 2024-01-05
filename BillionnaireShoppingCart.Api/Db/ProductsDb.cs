446cc882a03b6d4e49df770e07c0c63ddd189993
Chris Stephens <chris.stephens@appliedtechnology.se>
2024-01-05 17:48:31 +0100

    update: dbs

--- BillionnaireShoppingCart.Api/Db/ProductsDb.cs
+++ BillionnaireShoppingCart.Api/Db/ProductsDb.cs
@@ -10,6 +10,17 @@ public class ProductsDb : IDatabase<Product>
             Name = "Beer 0.4L",
             Price = 64,
             ImagePath = "https://www.spendrups.se/ConvertedImages/19e78202-73ef-4d31-b9d8-8e6fdad8dca3.png?format=jpg"
-        }
+        },
+        new () {
+            Name = "HotDog",
+            Price = 30,
+            ImagePath = "https://storage.googleapis.com/mediabucketpb/1/2021/02/Korv_960x640px.jpg"
+        },
+        new () {
+            Name = "Malmo FF",
+            Price = 700600000,
+            ImagePath = "https://www.mff.se/app/uploads/2019/03/190328_klubbskold-puff.jpg"
+        },
+        
     };
 }
